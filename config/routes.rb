827a78b674432b3d0ccd2fccd2b85d70e04f993e
Ryonyumura <rnyumura@gmail.com>
2019-03-25 18:56:53 +0900

    Make Static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+  get '/' , to:  'static_pages#index'
+  get 'static_pages/help',  to: 'static_pages#help'
+  get 'static_pages/about',  to: 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
