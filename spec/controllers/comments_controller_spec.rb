c7d662813fa0c158f649a445e2890e067d146f0a
Ryonyumura <rnyumura@gmail.com>
2019-04-05 04:03:44 +0900

    Add rspec

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe CommentsController, :type => :controller do
+  describe "GET index" do
+    it "assigns @teams" do
+    end
+
+    it "renders the index template" do
+    end
+  end
+end
---
+++ spec/controllers/opinions_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe OpinionsController, :type => :controller do
+  describe "GET index" do
+    it "" do
+    end
+
+    it "renders the index template" do
+    end
+  end
+end
